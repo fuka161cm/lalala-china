c7cda1ddc1cfd3f73acf77e8cefa89ab21f666f3
Fuka Ishihara <fuka.ishihara@gmail.com>
2021-05-29 14:20:53 +0900

    2021ss_01フォルダに画像追加

---
+++ pages/lookbook/2021ss_01.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="child-content">
+    <article class="article">
+      <h2 class="title is-2 sub-title">2021SS</h2>
+      <div class="lookbook">
+        <div class="lookbook look-block">
+            <div class="look-item">
+                <img src="../../assets/images/lookbook/2021ss_01/01.jpg" alt="">
+            </div>
+            <div class="look-item">
+                <img src="../../assets/images/lookbook/2021ss_01/02.jpg" alt="">
+            </div>
+            <div class="look-item">
+                <img src="../../assets/images/lookbook/2021ss_01/03.jpg" alt="">
+            </div>
+            <div class="look-item">
+                <img src="../../assets/images/lookbook/2021ss_01/04.jpg" alt="">
+            </div>
+            <div class="look-item">
+                <img src="../../assets/images/lookbook/2021ss_01/05.jpg" alt="">
+            </div>
+            <div class="look-item">
+                <img src="../../assets/images/lookbook/2021ss_01/06.jpg" alt="">
+            </div>
+            <div class="look-item">
+                <img src="../../assets/images/lookbook/2021ss_01/10.jpg" alt="">
+            </div>
+            <div class="look-item">
+                <img src="../../assets/images/lookbook/2021ss_01/08.jpg" alt="">
+            </div>
+            <div class="look-item">
+                <img src="../../assets/images/lookbook/2021ss_01/09.jpg" alt="">
+            </div>
+            <div class="look-item">
+                <img src="../../assets/images/lookbook/2021ss_01/07.jpg" alt="">
+            </div>
+        </div>
+      </div>
+    </article>
+   <article class="article">
+        <div class="lookbooks-table">
+            <div class="lookbooks-btn">
+                <nuxt-link to="/lookbook/portrait" class="link">
+                    <img src="../../assets/images/lookbook/btn/portrait-btn.jpg" alt="">
+                    <h3 class="title">Portrait</h3>
+                </nuxt-link>
+            </div>
+            <div class="lookbooks-btn">
+            <nuxt-link to="/lookbook/night" class="link">
+                    <img src="../../assets/images/lookbook/btn/night-btn.jpg" alt="">
+                    <h3 class="title">Night</h3>
+            </nuxt-link>
+            </div>
+        </div>
+   </article>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "2021ss_01",
+    head() {
+      return {
+        title: '2021SS / Look-book /'
+      }
+    }
+  }
+</script>

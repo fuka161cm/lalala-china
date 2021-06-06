f2dfd71ba777241bee5129c15d14312c33077713
Fuka Ishihara <fuka.ishihara@gmail.com>
2021-06-06 11:25:48 +0900

    lookbooksのリンクをふやした

--- pages/lookbooks.vue
+++ pages/lookbooks.vue
@@ -4,6 +4,24 @@
       <h2 class="title">Look-Book</h2>
       <div class="lookbooks-contents">
         <div class="lookbooks-btn">
+          <nuxt-link to="/lookbook/2021ss_01" class="link">
+            <img src="../assets/images/lookbook/btn/portrait-btn.jpg" alt="">
+            <h3 class="title">vivit</h3>
+          </nuxt-link>
+        </div>
+        <div class="lookbooks-btn">
+          <nuxt-link to="/lookbook/2021ss_02" class="link">
+            <img src="../assets/images/lookbook/btn/portrait-btn.jpg" alt="">
+            <h3 class="title">linen</h3>
+          </nuxt-link>
+        </div>
+        <div class="lookbooks-btn">
+          <nuxt-link to="/lookbook/2021ss_03" class="link">
+            <img src="../assets/images/lookbook/btn/portrait-btn.jpg" alt="">
+            <h3 class="title">はんそで</h3>
+          </nuxt-link>
+        </div>
+        <div class="lookbooks-btn">
           <nuxt-link to="/lookbook/portrait" class="link">
             <img src="../assets/images/lookbook/btn/portrait-btn.jpg" alt="">
             <h3 class="title">Portrait</h3>

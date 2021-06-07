23c12ef4c723146bf1b6e58451babe0a6a3775dd
Fuka Ishihara <fuka.ishihara@gmail.com>
2021-06-07 18:31:00 +0900

    topデザイン変更　キャッチコピー追加とlookbookへのリンク削除

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,12 @@
 <template>
   <div>
     <div class="hero-image"></div>
+    <h1 class="hero-text">
+      ラララチャイナ<br>
+      ふだん着のチャイナ服<br>
+    </h1>
     <article class="article">
-      <h3 class="title">Look-Book</h3>
-      <div class="top-lookbooks-contents">
-        <div class="top-lookbooks-btn">
-          <nuxt-link to="/lookbook/portrait" class="link">
-            <img src="../assets/images/lookbook/btn/portrait-btn.jpg" alt="">
-          </nuxt-link>
-        </div>
-        <nuxt-link to="/lookbooks" class="link-more">More</nuxt-link>
-      </div>
+
     </article>
   </div>
 </template>

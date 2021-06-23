1266eb56f7b7e070679780af50c09d9b8b28e1f3
Fuka Ishihara <fuka.ishihara@gmail.com>
2021-06-23 22:02:10 +0900

    不要なコード削除

--- pages/lookbooks.vue
+++ pages/lookbooks.vue
@@ -2,7 +2,6 @@
   <div class="child-content">
     <article class="article">
       <h2 class="title">ルックブック</h2>
-      <div class="lookbooks-contents">
         <div class="lookbooks-btn">
           <nuxt-link to="/lookbook/2021ss_01" class="link">
             <img src="../assets/images/lookbook/btn/2021ss_01.jpg" alt="">
@@ -25,20 +24,19 @@
         </div>
         <div class="lookbooks-btn">
           <nuxt-link to="/lookbook/portrait" class="link">
-            <img src="../assets/images/lookbook/btn/portrait-btn.jpg" alt="">
+            <img src="../assets/images/lookbook/portrait/14.jpg" alt="">
           </nuxt-link>
         </div>
         <div class="lookbooks-btn">
           <nuxt-link to="/lookbook/noon" class="link">
-            <img src="../assets/images/lookbook/btn/noon-btn.jpg" alt="">
+            <img src="../assets/images/lookbook/noon/05.jpg" alt="">
           </nuxt-link>
         </div>
         <div class="lookbooks-btn">
           <nuxt-link to="/lookbook/night" class="link">
-            <img src="../assets/images/lookbook/btn/night-btn.jpg" alt="">
+            <img src="../assets/images/lookbook/night/19.jpg" alt="">
           </nuxt-link>
         </div>
-      </div>
     </article>
   </div>
 </template>

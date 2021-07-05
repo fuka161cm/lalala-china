86a016a1e892935202ee69928fc27f706ac2c70d
Fuka Ishihara <fuka.ishihara@gmail.com>
2021-07-05 19:27:59 +0900

    top hero-text関連のコードとcss削除

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="hero">
     <div class="hero-image"></div>
-    <h1 class="hero-text">
-      ラララチャイナ<br>
-      ふだん着のチャイナ服<br>
-    </h1>
   </div>
 </template>
         
